b64214cf887e13ae9de9a5a07102dbbf868ce374
richwwin <richwei1208168@gmail.com>
2025-01-27 00:49:11 +0800

    simplify api to bare minimum

--- api/hello.js
+++ api/hello.js
@@ -1,16 +1,5 @@
-const handler = (req, res) => {
-  try {
-    res.status(200).json({
-      message: 'API is working!',
-      method: req.method,
-      url: req.url,
-      timestamp: new Date().toISOString()
-    });
-  } catch (error) {
-    res.status(500).json({
-      error: error.message
-    });
-  }
+const handler = (_, res) => {
+  res.status(200).send('Hello World');
 };
 
 module.exports = handler;
